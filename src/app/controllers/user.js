48a3fc00c981767d597457cfa8a46f51168fcae4
Valdecir Junior <valdecir.silva9@fatec.sp.gov.br>
2019-08-17 10:55:56 -0300

    Altered 'findById' to 'find by username'

--- src/app/controllers/user.js
+++ src/app/controllers/user.js
@@ -15,10 +15,11 @@ module.exports = {
     async show (request, response) {
         const { userId } = request.params;
         try {
-            const user = await User.findById( userId );
+            const user = await User.findOne({ username : userId });
     
             return response.status(200).send({ success: true, message: 'Service performed successfully', data: user });
         } catch (err) {
+            console.log(err);
             return response.status(500).send({ success: false, message: 'Error loading user', data: null });
         }
     },
